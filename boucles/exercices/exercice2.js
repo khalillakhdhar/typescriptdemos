03ca46188c003545cb79c2de097d565cb5d56b62
khalil <khalillakhdharatc@gmail.com>
2023-04-11 13:54:45 +0100

    pgcd

---
+++ boucles/exercices/exercice2.js
@@ -0,0 +1,19 @@
+/*
+a=5 , b=15
+pgcd(a,b)
+b=b-a=10
+b=b-a=5
+b-a==0 => pgcd=5
+
+*/
+function pgcd(a, b) {
+    console.log("pgcd(" + a + "," + b + ")");
+    while (a != b) {
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    }
+    return a;
+}
+console.log(pgcd(16, 48));
