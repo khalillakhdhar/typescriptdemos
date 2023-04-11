4278b2ad0294970d59a9335ecba93b691b15ed7f
khalil <khalillakhdharatc@gmail.com>
2023-04-11 10:09:18 +0100

    Update basique.ts

--- operations/basique.ts
+++ operations/basique.ts
@@ -1,15 +1,13 @@
 var num1:number = 10
-var num2:number = 2
-var
-res:number = 0
-res = num1 + num2
+var num2:number = 4
+var res:number = 0
+res = num1 + num2 // addition
 console.log("Sum:"+res);
-res = num1
-num2;
+res = num1-num2; // soustraction
 console.log("Difference: "+res)
-res = num1*num2
+res = num1*num2 // multiplication
 console.log("Product:"+res)
-res = num1/num2
+res = num1/num2 // division
 console.log("Quotient:"+res)
-res = num1%num2
+res = num1 % num2 // modulo
 console.log("Remainder:"+res)
