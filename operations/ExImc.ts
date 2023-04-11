18300aba0ce0962f27b8ae8e7c8c3c308105d407
khalil <khalillakhdharatc@gmail.com>
2023-04-11 10:39:14 +0100

    Update ExImc.ts

--- operations/ExImc.ts
+++ operations/ExImc.ts
@@ -2,4 +2,14 @@ function calculeIMC(poid:number,taille:number):number {
     return poid/(taille*taille);
 }
 const indice=Math.round(calculeIMC(80,1.80));
+
 console.log("Votre indice de masse corporelle est de "+indice);
+if(indice<20){
+    console.log("Vous êtes en dessous de votre poids idéal");
+}
+else if(indice>=20 && indice<=25){
+    console.log("Vous êtes dans votre poids idéal");
+}
+else{
+    console.log("Vous êtes en surpoids");
+}
