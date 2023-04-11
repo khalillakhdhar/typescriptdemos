1fc3ec804f30cf823f8cf2a7404a5034d7db9f7f
khalil <khalillakhdharatc@gmail.com>
2023-04-11 10:09:22 +0100

    Create basique.js

---
+++ operations/basique.js
@@ -0,0 +1,13 @@
+var num1 = 10;
+var num2 = 4;
+var res = 0;
+res = num1 + num2; // addition
+console.log("Sum:" + res);
+res = num1 - num2; // soustraction
+console.log("Difference: " + res);
+res = num1 * num2; // multiplication
+console.log("Product:" + res);
+res = num1 / num2; // division
+console.log("Quotient:" + res);
+res = num1 % num2; // modulo
+console.log("Remainder:" + res);
